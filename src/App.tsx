e4df01d5f0ea04707591f32ce328714e11306410
sugarmse <sarthak.sugar@gmail.com>
2024-12-12 17:56:29 +0545

    asym section desk view

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import "./App.css";
 import Mainpage from "./components/body/mainPage";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import Asym from "./components/body/asymSection";
 function App() {
 	return (
 		<>
 			<Header />
 			<Mainpage />
 			<Footer />
-			<Asym />
+			
 		</>
 	);
 }
